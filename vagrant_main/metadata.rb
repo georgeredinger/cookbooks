2e0e07b0ba508de87c6413d0c05944c79f64eddb
George Redinger <george@georgeredinger.com>
2012-03-10 12:33:44 -0800

    dotfiles

--- vagrant_main/metadata.rb
+++ vagrant_main/metadata.rb
@@ -5,8 +5,8 @@ description      "Installs/Configures vagrant_main"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-%w{ ack ctags curl  dotfiles git htop lsof  tmux tree
-    rbenv vim zsh }.each do |cookbook|
+%w{ ack ctags curl  git htop lsof  tmux tree
+    rbenv vim zsh nginx}.each do |cookbook|
   depends cookbook
 end
 
